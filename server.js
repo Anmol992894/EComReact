ad34dcfbe1aba850ae1b1d2839510b443dccae41
Anmol992894 <av992894@gmail.com>
2024-05-01 15:06:45 +0530

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require("dotenv").config();
-const PORT = 4000;
+const PORT = process.env || 5000;
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
